2139f5253698d7791cafc9744277a3156e952fc3
taka-kawa <ta6ka4shi@gmail.com>
2023-01-22 13:45:00 +0900

    update: run.py

--- src/run.py
+++ src/run.py
@@ -6,17 +6,21 @@ import click
 def controller():
     pass
 
+
 @controller.command()
 def train_valid():
     pass
+
 
 @controller.command()
 def valid():
     pass
 
+
 @controller.command()
 def submission():
     pass
+
 
 if __name__ == "__main__":
     controller()
